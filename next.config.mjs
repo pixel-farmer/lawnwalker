29a84d59d8793a80af354e1db9cc040da1e56569
pixel-farmer <cassie@pixel-farmers.com>
2025-07-18 21:44:17 -0400

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
